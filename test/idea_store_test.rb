e44de64798437c039f14e8193ed7d89c0f9a13e9
Alex Jensen <96dalex@gmail.com>
2014-10-28 11:33:55 -0600

    more idea store tests

--- test/idea_store_test.rb
+++ test/idea_store_test.rb
@@ -2,10 +2,42 @@ require_relative 'test_helper'
 require_relative '../lib/idea_box'
 
 class IdeaTest < Minitest::Test
+
+  TEST_PATH = "test/ideabox_save"
+
+  attr_reader :idea_store
+
+  def setup
+    File.delete TEST_PATH if File.exist? TEST_PATH
+    @idea_store = IdeaStore.new(TEST_PATH)
+  end
+
   def test_creates_path_if_doesnt_exist
-    path = "test/ideabox_save"
-    File.delete path if File.exist? path
-    IdeaStore.new(path).save
-    assert File.exist? path
+    idea_store.save
+    assert File.exist? TEST_PATH
+  end
+
+  def test_creates_ideas
+    idea_store.create("title" => "test_title", "description" => "test_description")
+    idea = idea_store.all.last
+    assert_equal "test_description", idea.description
+    assert_equal "test_title", idea.title
+  end
+
+  def test_creates_ideas_with_ids_if_not_given
+    idea_store.create("title" => "test_title", "description" => "test_description")
+    idea = idea_store.all.last
+    assert_equal 1, idea.id
+  end
+
+  def test_finds_by_id
+    idea_store.create("title" => "test_title", "description" => "test_description", "id" => 87)
+    idea = idea_store.all.last
+    assert_equal idea, idea_store.find("87")
+    refute idea_store.find("7")
+  end
+
+  def teardown
+    File.delete TEST_PATH if File.exist? TEST_PATH
   end
 end
